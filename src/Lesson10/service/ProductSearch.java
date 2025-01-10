248e40489dfcb57429c2489bf9da704e7c7981f9
Sergeant Svyatskiy <sithserega@gmail.com>
2025-01-10 19:30:34 +0300

    lesson 10 fix void methods

--- src/Lesson10/service/ProductSearch.java
+++ src/Lesson10/service/ProductSearch.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ProductSearch {
 
-    String searchByName(List<Product> list, String name);
+    Product searchByName(List<Product> list, String name);
 
     List<Product> searchByCost(List<Product> list, int cost);
 
