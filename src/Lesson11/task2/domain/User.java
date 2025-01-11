6bfef10cd1f47bc6e388b83983423db0d1a8f92c
Sergeant Svyatskiy <sithserega@gmail.com>
2025-01-11 15:55:41 +0300

    lesson 11 task 2

---
+++ src/Lesson11/task2/domain/User.java
@@ -0,0 +1,49 @@
+package Lesson11.task2.domain;
+
+import java.util.Objects;
+
+public class User implements Comparable<User>{
+
+    private String name;
+    private Integer age;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(User anotherUser) {
+
+        return age - anotherUser.age;
+    }
+}
